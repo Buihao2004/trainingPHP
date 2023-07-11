ff0f0da51fff62c3ab4dc2813bfa167f5d198e26
Buihao2004 <buihuyenhao09022004@gmail.com>
2023-07-11 13:30:09 +0700

    gioi thieu php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
   $_bienA ="Hello";
   $_bienB = 18;
+  echo $_bienB;
   var_dump($_bienA) ; //string
+  $ten = array("Hảo", "Lan","Hà");
+  print_r($ten);
+  $_bool = (boolean) false;
+  echo $_bool;
 
 ?>
