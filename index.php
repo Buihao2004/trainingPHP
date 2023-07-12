34f1d03778dd4b1d9224f4341136ea7e94e9a694
Buihao2004 <buihuyenhao09022004@gmail.com>
2023-07-12 16:24:57 +0700

    gioi thieu php

--- index.php
+++ index.php
@@ -7,5 +7,29 @@
   print_r($ten);
   $_bool = (boolean) false;
   echo $_bool;
+  
+  class Person {
+    public $name;
+    public $age;
+    public $gender;
+
+    public function __construct($name, $age, $gender) {
+        $this->name = $name;
+        $this->age = $age;
+        $this->gender = $gender;
+    }
+
+    public function introduce() {
+        echo "Hello, my name is " . $this->name . ". I am " . $this->age . " years old and I am " . $this->gender . ".";
+    }
+}
+
+// Tạo một object từ class Person
+$person = new Person("John", 25, "male");
+
+// Truy cập vào các thuộc tính và phương thức của object
+echo $person->name; // John
+
+$person->introduce(); // Hello, my name is John. I am 25 years old and I am male.
 
 ?>
