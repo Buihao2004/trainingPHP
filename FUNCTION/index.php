2884ab590ba27f97a7c5fadc6b81dc6db8547cae
Buihao2004 <buihuyenhao09022004@gmail.com>
2023-07-26 22:15:23 +0700

    function

--- FUNCTION/index.php
+++ FUNCTION/index.php
@@ -1,8 +1,50 @@
 <?php
 /*
 1. Hàm là gì?
-- là khối lệnh thực thi 1 nhiệm vụ 
+- là khối lệnh thực thi 1 nhiệm vụ và sử dingj 1 lần trong 1 CT
+- thực  thi khi được gọi
+- đầu vào => tham số, đầu ra = kết quả
+2. Khai báo hàm
+function tênhàm(DSTS1,....){
+    //khối lệnh thực thi
 
+} 
+* tên không phân biệt cái in hoa hay in thường
+* khi gọi hàm cần truyền đầy đủ tham số
+3. Tham số của hàm 
+- truyền dữ liệu đầu vào
+- tham số => 1 biến( chỉ sd trong hàm)
+4. trả về hàm
+- bằng RETURN
+5. Tham chiếu & tham trị 
+        Tham chiếu                                Tham trị
+    - chiếu tới vị trí bộ nhớ của biến khác         - mất đi khi không ở trong hàm
+    - truyền vị trí bộ nhớ
+    kí tự: "&"
+*/
+/*
+1. Sắp xếp
+sort() - sắp xếp các mảng theo thứ tự tăng dần.
+rsort() - sắp xếp các mảng theo thứ tự giảm dần.
+asort() - sắp xếp các mảng liên kết theo thứ tự tăng dần, theo giá trị.
+ksort() - sắp xếp các mảng liên kết theo thứ tự tăng dần, theo khóa.
+arsort() - sắp xếp các mảng liên kết theo thứ tự giảm dần, theo giá trị.
+krsort() - sắp xếp các mảng liên kết theo thứ tự giảm dần, theo khóa.
+2.Cắt 1 chuỗi ra thành chuỗi con
+- substr() hoặc explode()
+Hàm này được sử dụng để cắt các ký tự trong một chuỗi và chuyển nó thành dạng mảng.
+3. Tính tổng, tìm min, max giá trị của 1 mảng số nguyên
+- tìm min: min()
+- tìm Max: Max()
+- tính tổng: array_sum()
+4. Chuyển 1 mảng thành chuỗi và 1 chuỗi thành mảng
+- hàm implode():chuyển đổi một mảng thành một chuỗi trong PHP
+- hàm explode() :chuyển đổi một chuỗi thành một mảng trong PHP
+*print_r
+5. Xóa 1 phần tử trong mảng
+-  unset()
+*print_r
 
 */
+
 ?>
